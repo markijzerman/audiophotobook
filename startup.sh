c0f912729f09386618706b46b97b6d77a6874300
Mark IJzerman <info@markijzerman.com>
2017-07-21 11:19:22 +0900

    added startupscript

---
+++ startup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# This startup script will launch the python script & Pd with the right parameters. It is called with systemd.
+# More information here: http://www.raspberrypi-spy.co.uk/2015/10/how-to-autorun-a-python-script-on-boot-using-systemd/
+# Mark IJzerman, 2017
+
+pd ~/afbook/afbook.pd &
+python3 ~/afbook/findIDs.py
