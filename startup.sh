a2e5f9efae5e4386f104893171ac61e79aba8b03
Mark IJzerman <markijzerman@gmail.com>
2017-07-21 03:55:05 +0000

    commit again

--- startup.sh
+++ startup.sh
@@ -4,5 +4,5 @@
 # More information here: http://www.raspberrypi-spy.co.uk/2015/10/how-to-autorun-a-python-script-on-boot-using-systemd/
 # Mark IJzerman, 2017
 
-pd ~/afbook/afbook.pd &
-python3 ~/afbook/findIDs.py
+pd -nogui home/pi/afbook/afbook.pd &
+python3 home/pi/afbook/findIDs.py
